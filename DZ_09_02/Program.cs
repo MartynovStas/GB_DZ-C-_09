43b6d3d006472db5aaae9e526f0180764dd1b7cb
Stas Martynov <maks.mart71@bk.ru>
2023-03-15 01:39:28 +0400

    закомител домашние задание GB_C#_09

---
+++ DZ_09_02/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму 
+натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+int PriintText(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int NamberSumma(int n, int m)
+{
+    if (n == m)
+        return m;
+    else return n + NamberSumma(n + 1, m);
+}
+
+int namber = PriintText("Введите число n: ");
+int namber2 = PriintText("Введите число m: ");
+int summ = NamberSumma(namber, namber2);
+System.Console.WriteLine(summ);
+
+
+
---
+++ DZ_09_03/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29*/
+
+int PriintText(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int FunkchisAkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0) return FunkchisAkerman(m - 1, 1);
+    else if (m > 0 && n > 0) return FunkchisAkerman(m - 1, FunkchisAkerman(m, n - 1));
+
+    return FunkchisAkerman(m, n);
+}
+
+Console.WriteLine(FunkchisAkerman(PriintText("Введите число m"), PriintText("Введите число n")));
